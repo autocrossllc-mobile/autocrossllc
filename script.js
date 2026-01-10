8879c3dd66a5793c6d2144843df570fef26fb42d
autocrossllc-mobile <raevon.cross@gmail.com>
2026-01-10 14:40:27 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const hamburger = document.getElementById("hamburger");
 const navLinks = document.getElementById("navLinks");
 
 hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("show");
+navLinks.classList.toggle("show");
 });
