65e6f8e1f8d6e1f404d17bbcb45c9851f9f2668b
autocrossllc-mobile <raevon.cross@gmail.com>
2026-01-11 12:08:50 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ const mobileMenu = document.getElementById("mobileMenu");
 menuToggle.addEventListener("click", () => {
 mobileMenu.classList.toggle("active");
 });
+const lsCards = document.querySelectorAll(".ls-card");
+
+const revealCards = () => {
+lsCards.forEach(card => {
+const cardTop = card.getBoundingClientRect().top;
+const triggerPoint = window.innerHeight - 80;
+
+if (cardTop < triggerPoint) {
+card.classList.add("show");
+}
+});
+};
+
+window.addEventListener("scroll", revealCards);
+revealCards();
