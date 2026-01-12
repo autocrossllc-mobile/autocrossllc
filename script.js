a41e2ce43fed0baeae6ba90c53fd57bdaa4d4ca2
autocrossllc-mobile <raevon.cross@gmail.com>
2026-01-12 10:55:20 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const menuToggle = document.getElementById("menuToggle");
 const mobileMenu = document.getElementById("mobileMenu");
 
-menuToggle.addEventListener("click", () => {
+menuToggle.addEventListener("click", (e) => {
+e.stopPropagation();
 mobileMenu.classList.toggle("active");
+});
+
+/* TAP OUTSIDE TO CLOSE */
+document.addEventListener("click", (e) => {
+if (!mobileMenu.contains(e.target) && !menuToggle.contains(e.target)) {
+mobileMenu.classList.remove("active");
+}
 });
